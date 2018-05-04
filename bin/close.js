5ffb28b3528c2e7b8cc068b5e9ba3659e9251db5
Gorka <gorka@540deg.com>
2018-05-04 22:00:31 +0200

    On gjt-close ask removal of not fully merged branch

--- bin/close.js
+++ bin/close.js
@@ -15,39 +15,59 @@ var _config = require('./config');
 
 var _config2 = _interopRequireDefault(_config);
 
+var _inquirer = require('inquirer');
+
+var _inquirer2 = _interopRequireDefault(_inquirer);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const run = async issueTag => {
-    const spinner = (0, _ora2.default)();
+  const spinner = (0, _ora2.default)();
 
-    const branch = !issueTag ? await (0, _utils.requestBranch)() : await (0, _utils.searchIssueTagBranch)(issueTag);
+  const branch = !issueTag ? await (0, _utils.requestBranch)() : await (0, _utils.searchIssueTagBranch)(issueTag);
 
-    if (!branch) {
-        return spinner.fail('Branch not found');
-    }
+  if (!branch) {
+    return spinner.fail('Branch not found');
+  }
 
-    spinner.start('removing remote branch');
-    try {
-        await (0, _promise2.default)().silent(true).push(_config2.default.remote, `:${branch}`);
-        spinner.succeed('remote branch removed');
-    } catch (error) {
-        spinner.info(`remote branch not found, pruning ${_config2.default.remote}`);
-        await (0, _promise2.default)().raw(['remote', 'prune', _config2.default.remote]);
-    }
+  spinner.start('removing remote branch');
+  try {
+    await (0, _promise2.default)().silent(true).push(_config2.default.remote, `:${branch}`);
+    spinner.succeed('remote branch removed');
+  } catch (error) {
+    spinner.info(`remote branch not found, pruning ${_config2.default.remote}`);
+    await (0, _promise2.default)().raw(['remote', 'prune', _config2.default.remote]);
+  }
 
-    spinner.start('removing local branch');
+  spinner.start('removing local branch');
 
-    const status = await (0, _promise2.default)().status();
-    const currentBranch = status.current;
+  const status = await (0, _promise2.default)().status();
+  const currentBranch = status.current;
 
-    if (currentBranch === branch) {
-        spinner.info(`removing current branch, moving to ${_config2.default.mainBranch}`);
-        await (0, _promise2.default)().checkout(_config2.default.mainBranch);
-    }
+  if (currentBranch === branch) {
+    spinner.info(`removing current branch, moving to ${_config2.default.mainBranch}`);
+    await (0, _promise2.default)().checkout(_config2.default.mainBranch);
+  }
 
-    spinner.start('removing local branch');
-    await (0, _promise2.default)().deleteLocalBranch(branch);
+  spinner.start('removing local branch');
+  try {
+    await (0, _promise2.default)().silent(true).deleteLocalBranch(branch);
     spinner.succeed('local branch removed');
+  } catch (error) {
+    spinner.info('branch not fully merged');
+    const remove = (await _inquirer2.default.prompt([{
+      name: 'remove',
+      message: 'are you sure you want to close it (y or n)',
+      validate: answer => ['y', 'n'].includes(answer.toLowerCase())
+    }]))['remove'];
+
+    if (remove === 'y') {
+      await (0, _promise2.default)().raw(['branch', '-D', branch]);
+      spinner.succeed('local branch removed');
+    } else {
+      return spinner.fail('Won\'t remove branch, not fully merged');
+    }
+  }
 };
 
 run(process.argv[2]).catch(e => console.error(e));
--- src/close.js
+++ src/close.js
@@ -4,40 +4,57 @@ import git from 'simple-git/promise'
 import ora from 'ora';
 import { requestBranch, searchIssueTagBranch } from './utils';
 import config from './config';
+import inquirer from 'inquirer';
 
 const run = async (issueTag) => {
-    const spinner = ora()
+  const spinner = ora()
 
-    const branch = !issueTag
-        ? await requestBranch()
-        : await searchIssueTagBranch(issueTag)
+  const branch = !issueTag
+    ? await requestBranch()
+    : await searchIssueTagBranch(issueTag)
 
-    if (!branch) {
-        return spinner.fail('Branch not found');
-    }
+  if (!branch) {
+    return spinner.fail('Branch not found');
+  }
 
-    spinner.start('removing remote branch')
-    try {
-        await git().silent(true).push(config.remote, `:${branch}`)
-        spinner.succeed('remote branch removed')
-    } catch (error) {
-        spinner.info(`remote branch not found, pruning ${config.remote}`)
-        await git().raw(['remote', 'prune', config.remote])
-    }
+  spinner.start('removing remote branch')
+  try {
+    await git().silent(true).push(config.remote, `:${branch}`)
+    spinner.succeed('remote branch removed')
+  } catch (error) {
+    spinner.info(`remote branch not found, pruning ${config.remote}`)
+    await git().raw(['remote', 'prune', config.remote])
+  }
 
-    spinner.start('removing local branch')
+  spinner.start('removing local branch')
 
-    const status = await git().status()
-    const currentBranch = status.current
+  const status = await git().status()
+  const currentBranch = status.current
 
-    if (currentBranch === branch) {
-        spinner.info(`removing current branch, moving to ${config.mainBranch}`)
-        await git().checkout(config.mainBranch)
-    }
+  if (currentBranch === branch) {
+    spinner.info(`removing current branch, moving to ${config.mainBranch}`)
+    await git().checkout(config.mainBranch)
+  }
 
-    spinner.start('removing local branch')
-    await git().deleteLocalBranch(branch)
+  spinner.start('removing local branch')
+  try {
+    await git().silent(true).deleteLocalBranch(branch)
     spinner.succeed('local branch removed')
+  } catch (error) {
+    spinner.info('branch not fully merged')
+    const remove = (await inquirer.prompt([{
+      name: 'remove',
+      message: 'are you sure you want to close it (y or n)',
+      validate: answer => ['y', 'n'].includes(answer.toLowerCase())
+    }]))['remove']
+
+    if (remove === 'y') {
+      await git().raw(['branch', '-D', branch])
+      spinner.succeed('local branch removed')
+    } else {
+      return spinner.fail('Won\'t remove branch, not fully merged');
+    }
+  }
 }
 
 run(process.argv[2]).catch(e => console.error(e));
